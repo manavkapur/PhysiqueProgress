60fb8b703568839a053329d5c077bea44cbe4f06
Manav Kapur <62932155+manavkapur@users.noreply.github.com>
2026-01-05 09:52:20 +0530

    Notification code

--- PhysiqueProgress/App/AppDelegate.swift
+++ PhysiqueProgress/App/AppDelegate.swift
@@ -15,8 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseService.configure()
+        UNUserNotificationCenter.current().delegate = self
         return true
     }
+    
 
     // MARK: UISceneSession Lifecycle
 
@@ -33,5 +35,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler:
+        @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.banner, .sound])
+    }
+}
